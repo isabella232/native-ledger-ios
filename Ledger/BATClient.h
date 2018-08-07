6d684bddc5f8e5b3a4ab0fe68f90ac63ce5d7d89
Kyle Hickinson <kyle.hickinson@gmail.com>
2018-08-07 15:41:34 -0400

    Update copyrights

--- Ledger/BATClient.h
+++ Ledger/BATClient.h
@@ -1,10 +1,6 @@
-//
-//  BATClient.h
-//  Ledger
-//
-//  Created by Kyle Hickinson on 2018-06-14.
-//  Copyright © 2018 Kyle Hickinson. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #import <Foundation/Foundation.h>
 
